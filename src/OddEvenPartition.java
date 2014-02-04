220aac5ccaad8335775760fe5159a572fdfc2154
Yao <myspiritcrazy@gmail.com>
2014-02-04 14:27:39 -0800

    remove redudant code and update more complicated test case

--- src/OddEvenPartition.java
+++ src/OddEvenPartition.java
@@ -19,12 +19,6 @@ public class OddEvenPartition {
 		}
 	}
 	
-	public static void swap(int m, int n, int[] nums) {
-		int tmp = nums[m];
-		nums[m] = nums[n];
-		nums[n] = tmp;
-	}
-	
 	public static void main(String args[]) {
 		int[] nums = new int[]{1, 2, 3, 4, 5, 6};
 		System.out.println("Before partition: ");
@@ -38,7 +32,7 @@ public class OddEvenPartition {
 		
 		System.out.println("\n");
 		
-		int[] nums2 = new int[]{2, 4, 6, 1, 3, 5};
+		int[] nums2 = new int[]{0, 2, 8, 4, 7, 9, 1, 5, 5, 8};
 		System.out.println("Before partition: ");
 		for( int i = 0; i < nums2.length; i++ )
 			System.out.print(nums2[i] + " ");
