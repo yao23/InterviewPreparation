145289112c60becf9c3586376daf93d3d7ec0a15
Yao <myspiritcrazy@gmail.com>
2014-02-04 14:23:08 -0800

    remove redudant code

--- src/OddEvenPartition.java
+++ src/OddEvenPartition.java
@@ -17,25 +17,6 @@ public class OddEvenPartition {
 			else if( EvenIdx == -1 )
 				EvenIdx = i;
 		}
-/*				
-			if( nums[i] % 2 == 0 ) {
-				if( EvenIdx == 0 )
-					EvenIdx = i;
-				
-				while( i < nums.length - 1 && nums[i] % 2 == 0 ) 
-					i++;
-				
-				if( i < nums.length - 1 ) {
-					int OddNum = nums[i];
-					for( int j = i - 1; j >= EvenIdx; j-- ) 
-						nums[j + 1] = nums[j];
-					nums[EvenIdx] = OddNum;
-					EvenIdx++;
-				}
-				else
-					return;					
-			}
-*/			
 	}
 	
 	public static void swap(int m, int n, int[] nums) {
