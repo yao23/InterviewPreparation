07b79af885691eb4f476f80b5cf876af9de45fee
Yao <myspiritcrazy@gmail.com>
2014-02-15 23:12:03 -0800

    add Check String Rotation solution in Cracking the Code Interview Chapter 1 and problem 8

---
+++ src/IsRotation.java
@@ -0,0 +1,21 @@
+
+public class IsRotation {
+	private static boolean IsSubstring(String s1, String s2) {
+		for( int i = 0; i < (s1.length() - 1 - s2.length()); i++ )
+			if( s2.equals(s1.substring(i, i + s2.length() - 1)) )
+				return true;
+		return false;
+	}
+	
+	public static boolean CheckRotation(String s1, String s2) {
+		String tmp = s1 + s1;
+		return IsSubstring(tmp, s2);
+	}
+	
+	public static void main(String[] args) {
+		String s1 = "waterbottle";
+		String s2 = "erbottlewat";
+		System.out.println(s1 + " and " + s2 + 
+				(CheckRotation(s1, s2) ? " are " : " are not ") + "rotation");
+	}
+}
