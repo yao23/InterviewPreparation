3dc68e56839d165e68f1e5f509397ce42a580888
Yao <myspiritcrazy@gmail.com>
2014-02-15 17:39:00 -0800

    add a negative test case

--- src/IsPermutation.java
+++ src/IsPermutation.java
@@ -16,11 +16,16 @@ public class IsPermutation {
 		String s1_1 = "123456";
 		String s1_2 = "132654";
 		System.out.println(s1_1 + " and " + s1_2 + 
-				(CheckPermutation(s1_1, s1_2) ? " are " : " are not ") + "permutation");
+				(CheckPermutation(s1_1, s1_2) ? " are " : " are not ") + "permutations");
 		
 		String s2_1 = "123456";
 		String s2_2 = "13265";
 		System.out.println(s2_1 + " and " + s2_2 + 
-				(CheckPermutation(s2_1, s2_2) ? " are " : " are not ") + "permutation");
+				(CheckPermutation(s2_1, s2_2) ? " are " : " are not ") + "permutations");
+		
+		String s3_1 = "123456";
+		String s3_2 = "132657";
+		System.out.println(s3_1 + " and " + s3_2 + 
+				(CheckPermutation(s3_1, s3_2) ? " are " : " are not ") + "permutations");
 	}
 }
