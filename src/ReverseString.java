9562760b01ede919acf3492fb0ec7679c0b32c64
Yao <myspiritcrazy@gmail.com>
2014-02-15 16:14:46 -0800

    add Reverse String solution in Cracking the Code Interview Chapter 1 and problem 2

---
+++ src/ReverseString.java
@@ -0,0 +1,19 @@
+
+public class ReverseString {
+	public static String ReverseStr(String s) {
+		char[] result = new char[s.length()]; 
+		for( int i = 0; i < (s.length() >> 1); i++) {
+			result[i] = s.charAt(i);
+			result[s.length() - 1 - i] = s.charAt(s.length() - 1 - i);	
+		}
+		return new String(result);
+	}
+	public static void main(String args[]) {
+		String s1 = "abcdefghi";
+		String s2 = "missipi";
+		String s3 = "yongyuanziyoudexin";
+		System.out.println("Reversed " + s1 + " : " + ReverseStr(s1));
+		System.out.println("Reversed " + s2 + " : " + ReverseStr(s2));
+		System.out.println("Reversed " + s3 + " : " + ReverseStr(s3));
+	}
+}
