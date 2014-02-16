84b9eab27422f9f4ff6caa6b2fea9699f1ea590b
Yao <myspiritcrazy@gmail.com>
2014-02-15 17:51:29 -0800

    add a solution with extra space

--- src/IsPermutation.java
+++ src/IsPermutation.java
@@ -12,20 +12,49 @@ public class IsPermutation {
 		return s1.equals(TmpS2);
 	}
 	
+	public static boolean CheckPermutation2(String s1, String s2) {
+		if( s1.length() != s2.length() )
+			return false;
+		
+		int[] CharsInS1 = new int[26];
+		int[] CharsInS2 = new int[26];
+		
+		for( int i = 0; i < s1.length(); i++ ) {
+			CharsInS1[s1.charAt(i) - 'a']++;
+			CharsInS2[s2.charAt(i) - 'a']++;
+		}
+		for( int i = 0; i < CharsInS1.length; i++ ) {
+			if( CharsInS1[i] != CharsInS2[i] )
+				return false;
+		}
+		return true;
+	}
+	
 	public static void main(String[] args) {
-		String s1_1 = "123456";
-		String s1_2 = "132654";
+		String s1_1 = "abcdef";
+		String s1_2 = "acbfed";
 		System.out.println(s1_1 + " and " + s1_2 + 
 				(CheckPermutation(s1_1, s1_2) ? " are " : " are not ") + "permutations");
 		
-		String s2_1 = "123456";
-		String s2_2 = "13265";
+		String s2_1 = "abcdef";
+		String s2_2 = "acbfe";
 		System.out.println(s2_1 + " and " + s2_2 + 
 				(CheckPermutation(s2_1, s2_2) ? " are " : " are not ") + "permutations");
 		
-		String s3_1 = "123456";
-		String s3_2 = "132657";
+		String s3_1 = "abcdef";
+		String s3_2 = "acbfeg";
 		System.out.println(s3_1 + " and " + s3_2 + 
 				(CheckPermutation(s3_1, s3_2) ? " are " : " are not ") + "permutations");
+		
+		System.out.println("\n" + "Check Permutation with extra space: ");
+		
+		System.out.println(s1_1 + " and " + s1_2 + 
+				(CheckPermutation2(s1_1, s1_2) ? " are " : " are not ") + "permutations");
+		
+		System.out.println(s2_1 + " and " + s2_2 + 
+				(CheckPermutation2(s2_1, s2_2) ? " are " : " are not ") + "permutations");
+		
+		System.out.println(s3_1 + " and " + s3_2 + 
+				(CheckPermutation2(s3_1, s3_2) ? " are " : " are not ") + "permutations");
 	}
 }
