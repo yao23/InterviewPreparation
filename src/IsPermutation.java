495c215a8bc420f9f4ae326ebaab52435670a573
Yao <myspiritcrazy@gmail.com>
2014-02-15 17:37:02 -0800

    two strings are not permutations when the length are not same, add a tes case

--- src/IsPermutation.java
+++ src/IsPermutation.java
@@ -3,6 +3,9 @@ import java.util.Arrays;
 
 public class IsPermutation {
 	public static boolean CheckPermutation(String s1, String s2) {
+		if( s1.length() != s2.length() )
+			return false;
+		
 		char[] TmpStr2 = s2.toCharArray();
 		Arrays.sort(TmpStr2);
 		String TmpS2 = new String(TmpStr2); 
@@ -13,6 +16,11 @@ public class IsPermutation {
 		String s1_1 = "123456";
 		String s1_2 = "132654";
 		System.out.println(s1_1 + " and " + s1_2 + 
-				(CheckPermutation(s1_1, s1_2) ? " is " : " is not ") + "permutation");
+				(CheckPermutation(s1_1, s1_2) ? " are " : " are not ") + "permutation");
+		
+		String s2_1 = "123456";
+		String s2_2 = "13265";
+		System.out.println(s2_1 + " and " + s2_2 + 
+				(CheckPermutation(s2_1, s2_2) ? " are " : " are not ") + "permutation");
 	}
 }
