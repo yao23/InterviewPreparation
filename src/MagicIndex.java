7e1a67f98e51c96633445f05982db832e86153a7
Yao <myspiritcrazy@gmail.com>
2014-02-16 10:31:09 -0800

    add a negative test case'

--- src/MagicIndex.java
+++ src/MagicIndex.java
@@ -32,5 +32,19 @@ public class MagicIndex {
 			System.out.println("index: " + res + " " + "element: " + nums[res]);
 		else
 			System.out.println("No Magic Index!");
+		
+		int[] nums2 = new int[]{-40, -20, -1, 1, 2, 3, 
+				5, 8, 9, 12, 13};
+		System.out.println("Find a magic index whose " +
+					"value equals array element in 2nd array: ");
+		for( int i = 0; i < nums2.length; i++ )
+		System.out.println("index: " + i + " " + "element: " + nums2[i]);
+		
+		int res2 = FindMagicIndex(nums2);
+		System.out.println("Result: ");
+		if( res2 >= 0)
+			System.out.println("index: " + res2 + " " + "element: " + nums2[res2]);
+		else
+			System.out.println("No Magic Index!");
 	}
 }
