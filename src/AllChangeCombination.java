891ad78abc6bfd2868f8e9045bd036dcabaa5b00
Yao <myspiritcrazy@gmail.com>
2014-02-16 11:04:47 -0800

    add Calculate Changes(quarter, dime, nickel, penny) Combinations for a certain target solution in Cracking the Code Interview Chapter 9 and problem 8

---
+++ src/AllChangeCombination.java
@@ -0,0 +1,36 @@
+
+public class AllChangeCombination {
+	public static int CalChangeCombination(int[] changes, 
+									int idx, int target) {
+		if( changes[idx] == 1 )
+			return 1;
+		int NextIdx = 0, res = 0;
+		switch (changes[idx]) {
+			case 25:
+				NextIdx = 1;
+				break;
+			case 10:
+				NextIdx = 2;
+				break;
+			case 5:
+				NextIdx = 3;
+				break;
+			case 1:
+				NextIdx = 4;
+				break;				
+		}
+		for( int i = 0; (i * changes[idx]) <= target; i++ )
+			res += CalChangeCombination(changes, NextIdx, 
+										target - (i * changes[idx]));
+		return res;		
+	}
+	
+	public static void main(String[] args) {
+		int[] changes = new int[]{25, 10, 5, 1};
+		int target = 100;
+		System.out.println("Number of change combination for " + 
+							target + " is " + CalChangeCombination(changes, 0, target));
+							
+		
+	}
+}
