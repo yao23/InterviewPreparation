9e59a4fab11b8329ed88cbddb584cc682c88ee0e
Yao <myspiritcrazy@gmail.com>
2014-02-19 10:53:59 -0800

    add solution for Remove Duplicates from Unsorted Linkd List in Cracking the Code Interview Chapter 2 and problem 1

---
+++ src/RemoveDuplicatesFromUnsortedLinkedList.java
@@ -0,0 +1,61 @@
+import java.util.HashSet;
+
+
+public class RemoveDuplicatesFromUnsortedLinkedList {
+	public static ListNode RemoveDuplicates(ListNode l) {
+		HashSet<Integer> nodes = new HashSet<Integer>();
+		ListNode pre = null;
+		ListNode FakeHead = new ListNode(-1);
+		FakeHead.next = l;
+		
+		while( l != null ) {
+			if( nodes.contains(l.i) ) 
+				pre.next = l.next;
+			else {
+				nodes.add(l.i);
+				pre = l;
+			}
+			l = l.next;
+		}
+		
+		return FakeHead.next;
+	}
+	
+	public static void main(String[] args) {
+		ListNode l1 = new ListNode(1);	ListNode l2 = new ListNode(2);
+		ListNode l3 = new ListNode(1);	ListNode l4 = new ListNode(3);
+		ListNode l5 = new ListNode(4);	ListNode l6 = new ListNode(5);
+		ListNode l7 = new ListNode(2);	ListNode l8 = new ListNode(3);
+		l1.next = l2; l2.next = l3; l3.next = l4; l4.next = l5;
+		l5.next = l6; l6.next = l7; l7.next = l8; l8.next = null;
+		
+		System.out.println("Before removing: ");
+		l1.PrintList();
+		System.out.println();
+		System.out.println("After removing: ");
+		ListNode NewList = RemoveDuplicates(l1);
+		NewList.PrintList();
+
+	}
+	
+}
+
+class ListNode {
+	int i;
+	ListNode next;
+	
+	ListNode(int i) {
+		this.i = i;
+	}
+	@SuppressWarnings("unused")
+	public void PrintList() {
+		ListNode l = this;
+		if( l == null )
+			System.out.println("No node in list!");
+		
+		while( l != null ) {
+			System.out.print(l.i + " ");
+			l = l.next;
+		}
+	}
+}
