3aba60805343c53223986d02ec505af251e6fd5e
Yao <myspiritcrazy@gmail.com>
2014-02-19 10:04:21 -0800

    add Parenthesize Boolean Expression solution in Cracking the Code Interview Chapter 9 and problem 11

---
+++ src/ParenthesizeBooleanExpression.java
@@ -0,0 +1,82 @@
+import java.util.HashMap;
+
+
+public class ParenthesizeBooleanExpression {
+	public static int WaysToParenthesize(String exp, boolean res, int l, int r, 
+											HashMap<String, Integer> map) {
+		String key = "" + res + l + r; 
+		if( map.containsKey(key) )
+			return map.get(key);
+		if( l == r ) {
+			if( exp.charAt(l) == '1' && res )
+				return 1;
+			else if( exp.charAt(l) == '0' && !res )
+				return 1;
+			return 0;
+		}
+		int ways = 0;
+		if( res ) {
+			for( int i = l + 1; i <= r; i++ ) {
+				char op = exp.charAt(i);
+				switch(op) {
+					case '&':
+						ways += WaysToParenthesize(exp, true, l, i - 1, map) * 
+								WaysToParenthesize(exp, true, i + 1, r, map);
+						break;
+					case '|':
+						ways += WaysToParenthesize(exp, true, l, i - 1, map) * 
+								WaysToParenthesize(exp, true, i + 1, r, map);
+						ways += WaysToParenthesize(exp, true, l, i - 1, map) * 
+								WaysToParenthesize(exp, false, i + 1, r, map);
+						ways += WaysToParenthesize(exp, false, l, i - 1, map) * 
+								WaysToParenthesize(exp, true, i + 1, r, map);
+						break;
+					case '^':
+						ways += WaysToParenthesize(exp, true, l, i - 1, map) * 
+								WaysToParenthesize(exp, false, i + 1, r, map);
+						ways += WaysToParenthesize(exp, false, l, i - 1, map) * 
+								WaysToParenthesize(exp, true, i + 1, r, map);
+						break;
+				}
+			}
+		}
+		else {
+			for( int i = l + 1; i <= r; i++ ) {
+				char op = exp.charAt(i);
+				switch(op) {
+					case '&':
+						ways += WaysToParenthesize(exp, false, l, i - 1, map) * 
+								WaysToParenthesize(exp, false, i + 1, r, map);
+						ways += WaysToParenthesize(exp, true, l, i - 1, map) * 
+								WaysToParenthesize(exp, false, i + 1, r, map);
+						ways += WaysToParenthesize(exp, false, l, i - 1, map) * 
+								WaysToParenthesize(exp, true, i + 1, r, map);
+						break;
+					case '|':
+						
+						ways += WaysToParenthesize(exp, false, l, i - 1, map) * 
+								WaysToParenthesize(exp, false, i + 1, r, map);
+						break;
+					case '^':
+						ways += WaysToParenthesize(exp, true, l, i - 1, map) * 
+								WaysToParenthesize(exp, true, i + 1, r, map);
+						ways += WaysToParenthesize(exp, false, l, i - 1, map) * 
+								WaysToParenthesize(exp, false, i + 1, r, map);
+						break;
+				}
+			}
+		}
+		map.put(key,  ways);
+		return ways;
+	}
+	
+	public static void main(String[] args) {
+		String s1 = "1^0|0";
+		boolean b1 = true;
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		System.out.println(s1 + " has " + WaysToParenthesize(s1, b1, 0, s1.length() - 1, map) 
+							+ " ways to parenthesize to be " + (b1 ? "true" : "false"));
+		
+		
+	}
+}
