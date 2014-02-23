7d11eb856306083585f30b0a8ee395032e6811f4
Yao <myspiritcrazy@gmail.com>
2014-02-22 21:52:02 -0800

    add solution for validating a tree is subtree of another in Cracking the Code Interview chapter 4 problem 8

---
+++ src/IsSubtree.java
@@ -0,0 +1,21 @@
+
+public class IsSubtree {
+	public boolean TreeMatch(MyTreeNode t1, MyTreeNode t2) {
+		if( t1 == null && t2 == null )
+			return true;
+		else if( t1 == null || t2 == null )
+			return false;			
+		else if( t1.name == t2.name )
+			return (TreeMatch(t1.left, t2.right) && TreeMatch(t1.right, t2.right));
+		else
+			return false;
+	}
+	public boolean subtree(MyTreeNode t1, MyTreeNode t2) {
+		if( t2 == null )
+			return true;
+		if( t1.name == t2.name )
+			return TreeMatch(t1, t2);
+		else
+			return subtree(t1.left, t2) || subtree(t1.right, t2);
+	}
+}
