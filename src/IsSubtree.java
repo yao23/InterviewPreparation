d431c752d805f2235fcb250edfa23fd1eae5b739
Yao <myspiritcrazy@gmail.com>
2014-02-22 21:55:08 -0800

    keep validating left and right subtree when the root is not target

--- src/IsSubtree.java
+++ src/IsSubtree.java
@@ -15,9 +15,10 @@ public class IsSubtree {
 			return true;
 		if( t1 == null )
 			return false;
-		if( t1.name == t2.name )
-			return TreeMatch(t1, t2);
-		else
-			return subtree(t1.left, t2) || subtree(t1.right, t2);
+		if( t1.name == t2.name ) {
+			if( TreeMatch(t1, t2) )
+			return true;
+		}
+		return subtree(t1.left, t2) || subtree(t1.right, t2);
 	}
 }
