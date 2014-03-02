f23f780440a055d85ea8434045f05840762c956a
Yao <myspiritcrazy@gmail.com>
2014-03-01 19:11:54 -0800

    use decimal format for binary input

--- src/InsertInteger.java
+++ src/InsertInteger.java
@@ -1,20 +1,23 @@
 import java.math.BigInteger;
 
 public class InsertInteger {
-	private static BigInteger insertInteger(BigInteger n, int m, int i, int j) {
+	private static int insertInteger(int n, int m, int i, int j) {
 		int mask = ~( ((1 << (j - i + 1)) - 1) << i);
-		BigInteger tmpN = n.and(new BigInteger(Integer.toString(mask)));
-		return (tmpN.or(new BigInteger(Integer.toString(m << i))));
+		//System.out.println("Mask: " + mask);
+		//BigInteger tmpN = n.and(new BigInteger(Integer.toString(mask)));
+		int tmpN = n & mask;
+		return (tmpN | (m << i));
 	}
 	
 	public static void main(String[] args) {
-		BigInteger N = new BigInteger("10000000000");
-		int M = 10011;
+		//BigInteger N = new BigInteger("10000000000");
+		int M = 19; // 10011
 		int i = 2;
 		int j = 6;
+		int N = 1 << 10;
 		
 		System.out.println("Test case 1:");
-		System.out.println("Before inserting: " + N);
-		System.out.println("After inserting: " + insertInteger(N, M, i, j));
+		System.out.println("Before inserting: " + Integer.toBinaryString(N));
+		System.out.println("After inserting: " + Integer.toBinaryString(insertInteger(N, M, i, j)));
 	}
 }
