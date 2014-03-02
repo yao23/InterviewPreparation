5bd1e19853e67e3ede0a162a1657bbc856f907d5
Yao <myspiritcrazy@gmail.com>
2014-03-01 19:14:08 -0800

    use built in method to convert binary input to decimal int

--- src/InsertInteger.java
+++ src/InsertInteger.java
@@ -1,23 +1,19 @@
-import java.math.BigInteger;
 
 public class InsertInteger {
 	private static int insertInteger(int n, int m, int i, int j) {
 		int mask = ~( ((1 << (j - i + 1)) - 1) << i);
-		//System.out.println("Mask: " + mask);
-		//BigInteger tmpN = n.and(new BigInteger(Integer.toString(mask)));
 		int tmpN = n & mask;
 		return (tmpN | (m << i));
 	}
 	
 	public static void main(String[] args) {
-		//BigInteger N = new BigInteger("10000000000");
-		int M = 19; // 10011
 		int i = 2;
 		int j = 6;
-		int N = 1 << 10;
+		int N = Integer.parseInt("10000000000", 2); // binary to decimal 
+		int M = Integer.parseInt("10011", 2); 
 		
 		System.out.println("Test case 1:");
 		System.out.println("Before inserting: " + Integer.toBinaryString(N));
-		System.out.println("After inserting: " + Integer.toBinaryString(insertInteger(N, M, i, j)));
+		System.out.println("After inserting:  " + Integer.toBinaryString(insertInteger(N, M, i, j)));
 	}
 }
