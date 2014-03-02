aaf7f70f252420fcdc2c047c4f06b1706c0affa6
Yao <myspiritcrazy@gmail.com>
2014-03-02 11:10:36 -0800

    add a solution for converting a decimal double into binary format for CtCI chapter 5 problem 2

---
+++ src/DecimalToBinary.java
@@ -0,0 +1,29 @@
+
+public class DecimalToBinary {
+	private static String decimalToBinary(double d) {
+		if (d >= 1 || d <= 0) {
+			System.out.println("ERROR");
+		}
+		double frac = 0.5;
+		StringBuilder res = new StringBuilder();
+		while (d > 0) {
+			if (res.length() >= 32) {
+				System.out.println("ERROR");
+			}
+			if (d - frac > 0) {
+				res.append("1");
+				d -= frac;
+			} else {
+				res.append("0");
+			}
+			frac /= 2;
+		}
+		return res.toString();
+	}
+	public static void main(String[] args) {
+		double d = 0.72;
+		System.out.println("Test case 1");
+		System.out.println("Number: " + d + " binary:" + 
+				decimalToBinary(d));
+	}
+}
