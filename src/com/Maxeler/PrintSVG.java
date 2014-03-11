5df359ef439df5afa36f782fa77948f0f3e04f91
Yao <myspiritcrazy@gmail.com>
2014-03-10 17:04:02 -0700

    adjust bottom 4 layers

--- src/com/Maxeler/PrintSVG.java
+++ src/com/Maxeler/PrintSVG.java
@@ -115,13 +115,13 @@ public class PrintSVG {
 		String bot2 = addP(space(18 - 15, Direction.L) + space(35 - 33, Direction.D)) + "LULDLULD" + 
 					  addP(space(25 - 11, Direction.R)) + "RURDRURD";
 		String bot3 = addP(space(34 - 29, Direction.R) + space(33 - 28, Direction.D)) + "ULDLULDL" + 
-				      addP(space(31 - 23, Direction.L) + space(29 - 28, Direction.U)) + "JJJJJLMMMMMDLULDLUKKKKKNNNNN" + // small
-				      addP(space(18 - 10, Direction.L) + space(28 - 27, Direction.D)) + "LULDLULD";
-		String bot4 = addP(space(18 - 7, Direction.R) + "JJJJJRKKKKK" + space(28 - 28, Direction.D)) + "DRURDRUR" + // regular, adjust from 6 in first space() to 7
+				      addP(space(31 - 22, Direction.L) + space(29 - 28, Direction.U)) + "JJJJJLMMMMMDLULDLUKKKKKNNNNN" + // small
+				      addP(space(18 - 9, Direction.L) + space(28 - 27, Direction.D)) + "LULDLULD";
+		String bot4 = addP(space(18 - 6, Direction.R) + space(28 - 28, Direction.D)) + "DRURDRUR" + // regular
 			          addP(space(24 - 22, Direction.R) + space(26 - 24, Direction.D)) + "LLDDLLUULLDDLLUU"; // 2x large
-		String mid = addP(space(12 - 1, Direction.L) + space(24 - 21, Direction.D)) + "DRURDRUR" + // adjust from 16 in first space() to 12
-			         addP(space(14 - 5, Direction.R) + space(21 - 20, Direction.U)) + "DDDRRRUUURRRDDDRRRUUURRR" + // 3x large
-			         addP(space(35 - 26, Direction.R) + space(21 - 20, Direction.D)) + "RDRURDRU";
+		String mid = addP(space(16 - 1, Direction.L) + space(24 - 21, Direction.D)) + "DRURDRUR" + 
+			         addP(space(14 - 5, Direction.R) + "MMMMMUNNNNN"/*space(21 - 20, Direction.U)*/) + "DDDRRRUUURRRDDDRRRUUURRR" + // 3x large
+			         addP(space(35 - 26, Direction.R) + "MMMMMDNNNNN"/*space(21 - 20, Direction.D)*/) + "RDRURDRU";
 		String top4 = addP(space(39 - 26, Direction.L) + space(20 - 17, Direction.D)) + "LLLLDDDDLLLLUUUULLLLDDDDLLLLUUUU"; // 4x large, adjust from 28 to 26
 		String top3 = addP(space(17 - 5, Direction.L) + space(17 - 12, Direction.D)) + "DRURDRUR" + // adjust from 12 in first space() to 17
 				      addP(space(30 - 9, Direction.R)) + "RDRURDRU";
