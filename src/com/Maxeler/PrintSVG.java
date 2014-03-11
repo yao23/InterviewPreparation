e3cf7063e60aff22565ffd956c7be0948b60c046
Yao <myspiritcrazy@gmail.com>
2014-03-10 17:34:00 -0700

    remove commented helper code

--- src/com/Maxeler/PrintSVG.java
+++ src/com/Maxeler/PrintSVG.java
@@ -4,36 +4,7 @@ public class PrintSVG {
 	private static int len = 400; // image length (pixel)
 	private static int len0 = 10; // basic mode length
 	private static int len1 = 40; // mode 1 length
-/*	private static int p = 0; // point index
-	private static Point[] points = new Point[] {
-			new Point(0,0), new Point(0, 40), new Point(0, 360), new Point(0, 400), 
-			new Point(400, 400), new Point(400, 360), new Point(220, 350), new Point(180, 350), 
-			new Point(150, 330), new Point(110, 330), new Point(95, 280), new Point(55, 280), 
-			new Point(50, 205), new Point(10, 205), new Point(55, 120), new Point(95, 120), 
-			new Point(110, 70), new Point(150, 70), new Point(180, 50), new Point(220, 50), 
-			new Point(400, 0), new Point(400, 40), new Point(290, 70), new Point(250, 70), 
-			new Point(305, 120), new Point(345, 120), new Point(280, 170), new Point(120, 170), 
-			new Point(140, 210), new Point(260, 210), new Point(350, 205), new Point(390, 205), 
-			new Point(240, 240), new Point(160, 240), new Point(180, 260), new Point(220, 260), 
-			new Point(205, 270), new Point(185, 270), new Point(305, 280), new Point(345, 280), 
-			new Point(250, 330), new Point(290, 330)
-	};
 
-	private static Point[] points = new Point[] {
-		new Point(0,0), new Point(0, 40), new Point(0, 360), new Point(0, 400), 
-		new Point(400, 400), new Point(400, 360), new Point(220, 350), new Point(180, 350), 
-		new Point(110, 330), new Point(150, 330), new Point(95, 280), new Point(55, 280), 
-		new Point(50, 205), new Point(10, 205), new Point(55, 120), new Point(95, 120), 
-		new Point(110, 70), new Point(150, 70), new Point(180, 50), new Point(220, 50), 
-		new Point(400, 0), new Point(400, 40), new Point(290, 70), new Point(250, 70), 
-		new Point(305, 120), new Point(345, 120), new Point(280, 170), new Point(120, 170), 
-		new Point(140, 210), new Point(260, 210), new Point(350, 205), new Point(390, 205), 
-		new Point(240, 240), new Point(160, 240), new Point(180, 260), new Point(220, 260), 
-		new Point(205, 270), new Point(185, 270), new Point(305, 280), new Point(345, 280), 
-		new Point(250, 330), new Point(290, 330)
-	};
-*/
-	
 	private enum Direction {
 		U, D, L, R;
 	}
@@ -77,35 +48,8 @@ public class PrintSVG {
 	private static String addP(String s) {
 		return ("P" + s + "P");
 	}
-/*	
-	private static String proceed() {
-		String s = "";
-		int xDis = points[p + 1].getX() -  points[p + 2].getX();
-		int yDis = points[p + 1].getY() -  points[p + 2].getY();
-		if (xDis < 0) { // right
-			s += space(Math.abs(xDis), Direction.R);
-		} else if (xDis > 0) { // left
-			s += space(xDis, Direction.L);
-		}
-		if (yDis < 0) { // up
-			s += space(Math.abs(yDis), Direction.U);
-		} else if (yDis > 0) { // down
-			s += space(yDis, Direction.D);
-		}
-		p += 2; // next point
-		return s;
-	}
 	
 	private static String draw() {
-		String s = "";
-		s += mode(1);
-		s += proceed();
-		s += mode(1);
-		s += proceed();
-		return s;
-	}
-*/	
-	private static String draw2() {
 		String s = "";
 		int col1Space = (len - len1 * 2) / len0; 
 		String col1 = mode(1) + addP(space(col1Space, Direction.U)) + mode(1); // left column 1
@@ -120,8 +64,8 @@ public class PrintSVG {
 		String bot4 = addP(space(18 - 6, Direction.R) + space(28 - 28, Direction.D)) + "DRURDRUR" + // regular
 			          addP(space(24 - 22, Direction.R) + space(26 - 24, Direction.D)) + "LLDDLLUULLDDLLUU"; // 2x large
 		String mid = addP(space(16 - 1, Direction.L) + space(24 - 21, Direction.D)) + "DRURDRUR" + 
-			         addP(space(14 - 5, Direction.R) /*+ "MMMMMUNNNNN"space(21 - 20, Direction.U)*/) + "DDDRRRUUURRRDDDRRRUUURRR" + // 3x large
-			         addP(space(35 - 26, Direction.R) /*+ space(21 - 20, Direction.D)*/) + "RDRURDRU";
+			         addP(space(14 - 5, Direction.R)) + "DDDRRRUUURRRDDDRRRUUURRR" + // 3x large
+			         addP(space(35 - 26, Direction.R)) + "RDRURDRU";
 		String top4 = addP(space(39 - 28, Direction.L) + space(20 - 16, Direction.D)) + "LLLLDDDDLLLLUUUULLLLDDDDLLLLUUUU"; // 4x large, adjust from 16 in 2nd space() to 16
 		String top3 = addP(space(12 - 6, Direction.L) + space(17 - 12, Direction.D)) + "DRURDRUR" + // adjust 5 in 1st space() to 6 
 				      addP(space(30 - 10, Direction.R)) + "RDRURDRU"; // adjust 9 in 1st space() to 10 
@@ -137,26 +81,8 @@ public class PrintSVG {
 	
 	public static void main(String[] args) {
 		String s = "";
-		
-//		s += draw();
-		s += draw2();
+		s += draw();
 		s += "X"; // terminate drawing
 		System.out.println(s);
 	}
 }
-/*
-class Point {
-	private int x;
-	private int y;
-	Point(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-	public int getX() {
-		return x;
-	}
-	public int getY() {
-		return y;
-	}
-}
-*/
